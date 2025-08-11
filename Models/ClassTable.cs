48c0d1a686970687943698aec63675a3919de77b
Farhan Khan <khanjanfarhan2@gmail.com>
2025-08-11 18:23:02 +0500

    Relations Completed CRUD

---
+++ Models/ClassTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD.Models;
+
+public partial class ClassTable
+{
+    public int ClassId { get; set; }
+
+    public string ClassName { get; set; } = null!;
+
+    public virtual ICollection<CandidateTable> CandidateTables { get; set; } = new List<CandidateTable>();
+}
