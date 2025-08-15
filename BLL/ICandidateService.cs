51b51faf1d5c6365b6ca5af1b8b9a74441945c64
Farhan Khan <khanjanfarhan2@gmail.com>
2025-08-15 21:43:56 +0500

    3 Tier Architecture

---
+++ BLL/ICandidateService.cs
@@ -0,0 +1,12 @@
+﻿using CRUD.DAL.Dto;
+
+namespace CRUD.BLL.Services
+{
+    public interface ICandidateService
+    {
+        Task<CandidateResponseDto> AddCandidate(CandidateResponseDto dto);
+        Task<CandidateResponseDto?> UpdateCandidate(int id, CandidateResponseDto dto);
+        Task<List<CandidateResponseDto>> GetAllCandidates();
+        Task DeleteCandidate(int id);
+    }
+}
